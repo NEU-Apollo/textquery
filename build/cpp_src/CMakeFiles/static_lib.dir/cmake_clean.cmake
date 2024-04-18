90213561bbe746789400aea2e8a0301ef80fa94b
NEU-Apollo <123456789@example.com>
2024-04-19 00:05:02 +0800

    add build

---
+++ build/cpp_src/CMakeFiles/static_lib.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "../../lib/libstatic_lib.a"
+  "../../lib/libstatic_lib.pdb"
+  "CMakeFiles/static_lib.dir/AndQuery.cpp.o"
+  "CMakeFiles/static_lib.dir/AndQuery.cpp.o.d"
+  "CMakeFiles/static_lib.dir/NotQuery.cpp.o"
+  "CMakeFiles/static_lib.dir/NotQuery.cpp.o.d"
+  "CMakeFiles/static_lib.dir/OrQuery.cpp.o"
+  "CMakeFiles/static_lib.dir/OrQuery.cpp.o.d"
+  "CMakeFiles/static_lib.dir/Query.cpp.o"
+  "CMakeFiles/static_lib.dir/Query.cpp.o.d"
+  "CMakeFiles/static_lib.dir/QueryResult.cpp.o"
+  "CMakeFiles/static_lib.dir/QueryResult.cpp.o.d"
+  "CMakeFiles/static_lib.dir/TextQuery.cpp.o"
+  "CMakeFiles/static_lib.dir/TextQuery.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/static_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
